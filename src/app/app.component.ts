2f7e16ee51b4496c87c5a10abdcbdfc5eb2c6367
pakapontong <pakapon.tong50@gmail.com>
2019-02-10 14:34:11 +0700

    'eiei'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Card } from './card';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  cards: Card[] = []; //เป็น array ของ card
+
+  get sorted(): Card[]{
+    console.log('access');
+    return this.cards.sort( (a, b) => b.votes - a.votes);
+
+  }
+
+
+  onclick(firstname:HTMLInputElement,lastname:HTMLInputElement){
+    //console.log(firstname.value+" "+lastname.value)
+    const card = new Card(firstname.value,lastname.value,0);
+    this.cards.push(card); //.push ยัดค่าไว้ใน array ค่าจะไปต่อท้าย 
+    console.log(this.cards);
+    firstname.value = '';
+    lastname.value = '';
+  }
 }
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Card } from '../card';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+  @Input() card:Card;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  upvotes(){
+    this.card.votes++;
+  }
+  downvotes(){
+    this.card.votes--;
+  }
+  
+}
